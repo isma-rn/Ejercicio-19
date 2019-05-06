650b1e539ed6a599804b13cdaf34570286a286ad
Juan Ismael Rosas Nava <JIsmael-RN@outlook.com>
2019-05-06 09:52:19 -0500

    Inicio

---
+++ E19_CuadradoPerfecto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E19_CuadradoPerfecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeros cuadrados perfectos");
+            for (int i = -2; i <= 400; i++)
+                if(EsCuadradoPerfecto2(i))
+                    Console.WriteLine("[{0}] ", i);
+            int n = 3;
+            Console.WriteLine("{0} {1} cuadrado perfecto", n, EsCuadradoPerfecto(n)? "es" : "no es" );
+            Console.ReadLine();
+        }
+        public static bool EsCuadradoPerfecto(int n)
+        {
+            if (n > 0)            
+                for(int i = 1; i<=n; i++)                
+                    if (i * i == n)
+                        return true;           
+            return false;
+        }
+
+        public static bool EsCuadradoPerfecto2(int n)
+        {
+            return (n >= 0 && Math.Pow( (int)Math.Sqrt(n), 2) == n)? true : false;
+        }
+    }
+}
